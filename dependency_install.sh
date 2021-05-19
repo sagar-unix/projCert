3b4f007c85278eb62e35a1d81376f196cca59be3
SAGAR Nair <sagar.nair@pawsforlife.com.au>
2021-05-19 10:03:15 +0530

    Adding my Selenium dependency installation script

---
+++ dependency_install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#Author: SAGAR
+#ID : sagar-unix
+#Designation: DevOps Engineer
+set -x 
+cd /tmp/
+yum -y erase google-chrome-stable || true
+rm -rf /opt/google/chrome || true
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+yum -y localinstall google-chrome-stable_current_x86_64.rpm
+rm google-chrome-stable_current_x86_64.rpm
+chrome_version=$(yum info  google-chrome-stable |grep -e "Version\b.*[d\.]*"|cut -d ':' -f2|cut -d "." -f1,2,3|awk '{$1=$1};1')
+exact_chrome_version=$(curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${chrome_version})
+rm chromedriver_linux64.zip
+wget  https://chromedriver.storage.googleapis.com/${exact_chrome_version}/chromedriver_linux64.zip
+unzip -o chromedriver_linux64.zip -d /opt/google/chrome/
+chmod a+x /opt/google/chrome/chromedriver
+ln -s /opt/google/chrome/chromedriver /usr/bin/chromedriver
+exit 0
